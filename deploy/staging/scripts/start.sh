bb42157b30936efc1b14d133aafcc13333a9c233
otaigbe <otaigbe@fisshboneandlestr.com>
2023-07-25 21:15:27 +0100

    tweaking manifest file

--- deploy/staging/scripts/start.sh
+++ deploy/staging/scripts/start.sh
@@ -3,5 +3,6 @@
 # cd /home/ubuntu/vas-vendor/  
 # npm install 
 # npm run build
+source ~/.nvm/nvm.sh
 cd /home/ubuntu/vas-vendor/startup/
 /home/ubuntu/.nvm/versions/node/v16.14.0/bin/pm2 startOrRestart staging.config.js
