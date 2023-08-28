800cb9a660d6f296375ed1a8a180d4a70188595a
Chia Gabriel <gabrielsonchia@gmail.com>
2023-08-28 19:06:05 +0100

    Modified config file to use custome env method

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,14 +7,12 @@ import { ErrorHandler } from "@weaverkit/errors";
 const ENV = process.env.NODE_ENV || "development";
 const PROD = ENV === "production";
 const OVERRIDE_PATH = path.resolve(__dirname, `./../../.${ENV}.env`);
-console.log("VALID PATH", OVERRIDE_PATH);
 if (fs.existsSync(OVERRIDE_PATH)) {
 	loadOverrides({
 		path: OVERRIDE_PATH,
 		debug: true,
 	});
 }
-// console.log("PROCESS ENV", process.env);
 function getEnv<T = string>(key: string, onNotExist: any | null = null) {
 	return (process.env[key] || onNotExist) as T;
 }
@@ -23,7 +21,6 @@ const LOG_DIR = path.resolve(process.env.LOG_DIR || __dirname + "/../../logs");
 const getLogPath = () => {
 	return path.resolve(LOG_DIR, path.parse(process.env.pm_exec_path || process.argv[1]).name);
 };
-
 const Config = Object.freeze({
 	App: {
 		NAME: "Vas Vendor",
@@ -36,7 +33,7 @@ const Config = Object.freeze({
 		contactEmail: getEnv<string>("CONTACT_EMAIL"),
 		contactPhone: getEnv<string>("CONTACT_PHONE"),
 		LOG_TO_FILE: !!Number(getEnv<string>("ADD_FILE_LOGGING", "0")),
-		BASE_URL: getEnv<string>("BASE_URL") || "http://127.0.0.1:4600/",
+		BASE_URL: getEnv<string>("BASE_URL", "http://127.0.0.1:4600/"),
 		JWT_SECRET: getEnv<string>("JWT_SECRET"),
 	},
 	Fela: {
@@ -53,23 +50,23 @@ const Config = Object.freeze({
 		apiKey: getEnv<string>("PHEDC_API_KEY"),
 	},
 	Redis: {
-		host: process.env.REDIS_HOST || "127.0.0.1",
-		port: Number(process.env.REDIS_PORT) || 6379,
-		password: process.env.REDIS_PASSWORD || "",
-		user: process.env.REDIS_USER,
-		db: process.env.REDIS_DB || 2,
-		// tls: process.env.REDIS_TLS,
+		host: getEnv<string>("REDIS_HOST", "127.0.0.1"),
+		port: Number(getEnv<string>("REDIS_PORT", 6379)),
+		password: getEnv<string>("REDIS_PASSWORD", ""),
+		user: getEnv<string>("REDIS_USER"),
+		db: getEnv<string>("REDIS_DB", 2),
 	},
 	MySQL: {
-		database: process.env.MYSQL_DATABASE,
-		username: process.env.MYSQL_USERNAME,
-		password: process.env.MYSQL_PASSWORD,
-		host: process.env.MYSQL_HOST,
-		port: Number(process.env.MYSQL_PORT),
+		database: getEnv<string>("MYSQL_DATABASE"),
+		username: getEnv<string>("MYSQL_USERNAME"),
+		password: getEnv<string>("MYSQL_PASSWORD"),
+		host: getEnv<string>("MYSQL_HOST"),
+		port: Number(getEnv<string>("MYSQL_PORT")),
+		systemUser: getEnv<string>("SYSTEM_USER", 1),
 		dialet: "mysql",
 	},
 	Db: {
-		MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/vasvending",
+		MONGO_URI: getEnv<string>("MONGO_URI", "mongodb://localhost:27017/vasvending"),
 	},
 });
 
